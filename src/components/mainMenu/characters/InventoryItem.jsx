231a55df36211aab95c0f29e6ea1e906725cfeda
hailey-tran-9 <101952089+hailey-tran-9@users.noreply.github.com>
2025-06-26 18:18:49 -0700

    [Characters] show pack contents

--- src/components/mainMenu/characters/InventoryItem.jsx
+++ src/components/mainMenu/characters/InventoryItem.jsx
@@ -6,43 +6,62 @@ export default function InventoryItem({ item, ...props }) {
     variables: { index: item.index },
   });
   let content;
+  let isPack = false;
   if (data) {
     const equipmentData = data.equipment;
-    console.log("inventory item equipment data:", equipmentData);
+    // console.log("inventory item equipment data:", equipmentData);
 
-    if (equipmentData["equipment_category"].index === "armor") {
+    if (
+      equipmentData["gear_category"] &&
+      equipmentData.index.includes("-pack")
+    ) {
+      isPack = true;
       content = (
-        <div className="flex flex-col text-[1rem]">
-          <p>{equipmentData.desc}</p>
-          <p>Armor Category: {equipmentData["armor_category"]}</p>
-          <p>AC: {equipmentData["armor_class"].base}</p>
-          <p>
-            DEX Bonus:{" "}
-            {equipmentData["armor_class"]["dex_bonus"] ? "True" : "False"}
-          </p>
-          <p>Max Bonus: {equipmentData["armor_class"]["max_bonus"]}</p>
-          <p>
-            Stealth Disadvantage:{" "}
-            {equipmentData["stealth_disadvantage"] ? "True" : "False"}
-          </p>
+        <div className="max-h-[25vh] overflow-y-auto flex flex-col text-[1rem] gap-y-3 mt-2">
+          {equipmentData.contents.map((packItem, packItemIndex) => (
+            <InventoryItem
+              item={{ ...packItem.item, quantity: packItem.quantity }}
+              key={props.key + "-packItem-" + packItemIndex}
+            />
+          ))}
         </div>
       );
-    } else if (equipmentData["equipment_category"].index === "weapon") {
-      content = (
-        <div className="flex flex-col text-[1rem]">
-          <p>{equipmentData.desc}</p>
-          <p>Weapon Category: {equipmentData["weapon_category"]}</p>
-          <p>
-            Weapon Range: {equipmentData["weapon_range"]}{" "}
-            {equipmentData.range.normal}
-          </p>
-        </div>
-      );
-    } else if (equipmentData["equipment_category"].index === "tool") {
-    } else if (equipmentData["equipment_category"].index === "gear") {
-    } else if (equipmentData["equipment_category"].index === "pack") {
-    } else if (equipmentData["equipment_category"].index === "ammunition") {
-    } else if (equipmentData["equipment_category"].index === "vehicle") {
+    } else {
+      if (equipmentData["equipment_category"].index === "armor") {
+        content = (
+          <div className="flex flex-col text-[1rem]">
+            <p>{equipmentData.desc}</p>
+            <p>{equipmentData["armor_category"]}</p>
+            <p>{`AC ${equipmentData["armor_class"].base}, ${
+              equipmentData["armor_class"]["dex_bonus"] && "DEX Bonus"
+            }, ${
+              equipmentData["armor_class"]["max_bonus"] > 0 &&
+              `Max Bonus ${equipmentData["armor_class"]["max_bonus"]}`
+            }`}</p>
+            {equipmentData["stealth_disadvantage"] && (
+              <p>Stealth Disadvantage</p>
+            )}
+          </div>
+        );
+      } else if (equipmentData["equipment_category"].index === "weapon") {
+        content = (
+          <div className="flex flex-col text-[1rem]">
+            <p>{equipmentData.desc}</p>
+            <p>
+              {`${equipmentData["category_range"]}, ${equipmentData.range.normal}ft`}
+            </p>
+            <p>{`${equipmentData.damage["damage_dice"]} ${equipmentData.damage["damage_type"].name}`}</p>
+            {equipmentData["two_handed_damage"] && (
+              <p>{`Two-handed Damage: `}</p>
+            )}
+          </div>
+        );
+      } else if (equipmentData["equipment_category"].index === "tool") {
+      } else if (equipmentData["equipment_category"].index === "gear") {
+      } else if (equipmentData["equipment_category"].index === "pack") {
+      } else if (equipmentData["equipment_category"].index === "ammunition") {
+      } else if (equipmentData["equipment_category"].index === "vehicle") {
+      }
     }
 
     // content = (
@@ -61,7 +80,7 @@ export default function InventoryItem({ item, ...props }) {
         </div>
       </div>
       {content}
-      <div className="h-[2px] bg-[#4a4a4aad] mt-8" />
+      {!isPack && <div className="h-[2px] bg-[#4a4a4aad] mt-8" />}
     </div>
   );
 }
